851b1a34f3a2e69864af652641327ff19dec52a7
Harshwardhan9431 <wardhanharsh732@gmail.com>
2023-12-28 16:47:04 +0530

    Initial Commit

---
+++ src/components/Messages.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Messages() {
+  return (
+    <div>
+      <Container>
+        Messages
+      </Container>
+    </div>
+  )
+}
+
+export default Messages
+
+const Container = styled.div`
+height: 80%;
+`;
+
